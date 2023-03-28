248ba6f0668f30d64383268810a08f6c3dc912dd
gregbilletdeaux <121880999+gregbilletdeaux@users.noreply.github.com>
2023-03-28 18:28:43 -0500

    Feature/auth connect (#2)
    * feat(auth): implement Auth Connect with Identity Vault

--- src/app/core/session-vault/session-vault.service.mock.ts
+++ src/app/core/session-vault/session-vault.service.mock.ts
@@ -1,6 +1,12 @@
+import { SessionVaultService } from './session-vault.service';
+
 export const createSessionVaultServiceMock = () =>
-  jasmine.createSpyObj('SessionVaultService', {
-    login: Promise.resolve(),
-    restoreSession: Promise.resolve(),
-    logout: Promise.resolve(),
+  jasmine.createSpyObj<SessionVaultService>('SessionVaultService', {
+    clearSession: Promise.resolve(),
+    canUnlock: Promise.resolve(false),
+    getSession: Promise.resolve(null),
+    setSession: Promise.resolve(),
+    isLocked: Promise.resolve(false),
+    setUnlockMode: Promise.resolve(),
+    unlock: Promise.resolve(),
   });
